9d6757c51f61d240bed4442a476c927dc2b2e164
Mohamed <i.freedomheart111@gmail.com>
2024-05-08 17:25:50 +0200

    typing effect

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,16 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 
+
 function App() {
   
+  function displaytext(text){
+    const letter = text.split('').map((letter,idx)=>
+    <span key={idx} style={{animationDelay: `${0.5*idx}s`}} className='fadein'>
+      {letter}
+    </span>
+    )
+  }
 
   return (
     <>
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Hero.css'
 import { getImageUrl } from '../../utils'
 
+const text1="Hi , I'm Mohamed"
+const text2="welcome to my portfolio"
+
+
+
+
+
 const Hero = () => {
+  const [text,settext]=useState(text1)
+  const [displayText, setDisplayText] = useState('');
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    // Clear previous timeouts and intervals
+    const intervalId = setInterval(() => {
+      if (index < text.length) {
+        setDisplayText(displayText => displayText + text[index]);
+        setIndex(index => index + 1);
+      } else {
+        clearInterval(intervalId);
+        setDisplayText('');
+        setIndex(0);
+        settext(txt=>{
+          if(txt===text1) return text2;
+          else return text1;
+        })
+
+         // Stop the interval when all letters are displayed
+      }
+    }, 500); // Display each letter after 500ms
+
+    return () => clearInterval(intervalId); // Cleanup interval on component unmount
+  }, [index, text]); 
   return (
     <section className='container'>
         <div className='content'>
-            <h2 className='Title'>Hi , I'm Mohamed</h2>
-            <h1 className='descr'>welcome to my portfolio </h1>
-            <a className='contact'>contact me!</a>
+            <h2 className='Title'>{displayText}</h2>
+            
+            <span className='descr'>{text2}</span>
+            <a className='contact'>contasct me!</a>
         </div>
         <img src={getImageUrl("hero/heroImage.png")} className='heroimg'>
         </img>
