e12f590acd670e0d54b694e7042d786481e1d218
Mohamed <i.freedomheart111@gmail.com>
2024-05-11 15:50:23 +0200

    add cards

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -7,38 +7,33 @@ const Projects = () => {
         { title: "Card 1", content: "This is the content for Card 1." },
         { title: "Card 2", content: "This is the content for Card 2." },
         { title: "Card 3", content: "This is the content for Card 3." },
+        { title: "Card 1", content: "This is the content for Card 1." },
+        { title: "Card 2", content: "This is the content for Card 2." },
+        { title: "Card 3", content: "This is the content for Card 3." },
+        { title: "Card 1", content: "This is the content for Card 1." },
+        { title: "Card 2", content: "This is the content for Card 2." },
+        { title: "Card 3", content: "This is the content for Card 3." },
       ];
 
-      const containerRef = useRef(null);
-      useEffect(() => {
-        const container = containerRef.current;
-        if (!container) return; 
       
-        const handleScroll = () => {
-          const { scrollTop } = container;
-          const cards = container.querySelectorAll('.card');
-        
-          cards.forEach((card, index) => {
-            if (scrollTop > (index * 100)) {
-              card.style.transform = `translateY(${scrollTop - index * 100}px)`;
-              card.style.zIndex = 1000 - index;
-            }
-          });
-        };
-        
-        container.addEventListener('scroll', handleScroll);
-        return () => container.removeEventListener('scroll', handleScroll);
-      }, []);
 
       return (
-        <div ref={containerRef} className='projects-container'>
+        <div  className='projects-container'>
           <h1 className='projects-title'>My projects</h1>
-          {cardData.map((card, index) => (
-            <div className='card' key={index}>
-              <h2>{card.title}</h2>
-              <p>{card.content}</p>
-            </div>
-          ))}
+          <div className='grid'>
+          
+          {cardData.map((project)=>{
+            return (
+            <>
+              <div className='card'>
+              <h1 className='project-title'>{project.title}</h1>
+              
+              <p className='project-content'>{project.content}</p>
+              </div>
+            </>
+            )
+          })}
+          </div>
         </div>
       );
 }
