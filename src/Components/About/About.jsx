01ce543f1f9ec2a4a354e258670af9d5c5569ccc
Mohamed <i.freedomheart111@gmail.com>
2024-05-20 17:46:10 +0200

    finalisation

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,28 +6,31 @@ const About = () => {
   return (
     <section className='container1'>
         <h2 className='title1'>About</h2>
+        <h2 className='desc'>Hello, my name is Mohamed. I am a 21-year-old Moroccan engineering student at Centrale Lille, one of the top 10 French engineering schools. I am interested in web development and data science.</h2>
         <div className='content1'>
+            
             <img src={getImageUrl("about/aboutImage.png")}className='aboutImg'></img>
             <ul className='aboutitems'>
+                <h1 className='interests'>my main interests</h1>
                 <li className='aboutitem'>
                     <img src={getImageUrl("about/cursorIcon.png")}></img>
                     <div className='aboutitemtext'>
                         <h3> Front end development</h3>
-                        <p> description </p>
+                        <p> with React.js and Next.js  </p>
                     </div>
                 </li>
                 <li className='aboutitem'>
                     <img src={getImageUrl("about/serverIcon.png")}></img>
                     <div className='aboutitemtext'>
                         <h3> Back end development</h3>
-                        <p> description </p>
+                        <p> with django  </p>
                     </div>
                 </li>
                 <li className='aboutitem'>
                     <img src={getImageUrl("about/uiIcon.png")}></img>
                     <div className='aboutitemtext'>
                         <h3> Data Science</h3>
-                        <p> description </p>
+                        <p> with python  </p>
                     </div>
                 </li>
             </ul>
